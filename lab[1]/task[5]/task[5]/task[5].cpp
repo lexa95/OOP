03bb423584f28b37296740c62e3d27a92a4fb28c
lexa95 <lexashar@gmail.com>
2015-03-03 00:12:13 +0300

    edit

--- lab[1]/task[5]/task[5]/task[5].cpp
+++ lab[1]/task[5]/task[5]/task[5].cpp
@@ -10,7 +10,51 @@ int StringToInt(const char * str, bool & err)
 	return param;
 }
 
-bool Ñrypt(const char * nameInputFile, const char * nameOutputFile, const int key)
+int StirBits(int bit)
+{
+	int result = 0;
+
+	result |= (bit & 1) << 2;
+	result |= (bit & 2) << 2;
+	result |= (bit & 4) << 2;
+	result |= (bit & 8) << 3;
+	result |= (bit & 16) << 3;
+	result |= (bit & 32) >> 5;
+	result |= (bit & 64) >> 5;
+	result |= (bit & 128) >> 2;
+
+	return result;
+}
+
+int BackStirBits(int bit)
+{
+	int result = 0;
+
+	result |= (bit & 1) << 5;
+	result |= (bit & 2) << 5;
+	result |= (bit & 4) >> 2;
+	result |= (bit & 8) >> 2;
+	result |= (bit & 16) >> 2;
+	result |= (bit & 32) << 2;
+	result |= (bit & 64) >> 3;
+	result |= (bit & 128) >> 3;
+
+	return result;
+}
+
+void Ñrypt(int ch, int key, FILE * f)
+{
+	fputc(StirBits(ch ^ key), f);
+}
+
+void Decrypt(int ch, int key, FILE * f)
+{
+
+	fputc(BackStirBits(ch) ^ key, f);
+}
+
+bool WriteFromFileInFile(const char * nameInputFile, const char * nameOutputFile,
+	const int key, void(*Process)(int ch, int key, FILE * f))
 {
 	FILE *pInputFile = fopen(nameInputFile, "rb");
 	if (pInputFile == NULL)
@@ -29,7 +73,7 @@ bool Ñrypt(const char * nameInputFile, const char * nameOutputFile, const int ke
 	int ch;
 	while ((ch = fgetc(pInputFile)) != EOF)
 	{
-		fputc(ch ^ key, pOutputFile);
+		Process(ch, key, pOutputFile);
 	}
 
 	fclose(pInputFile);
@@ -39,6 +83,32 @@ bool Ñrypt(const char * nameInputFile, const char * nameOutputFile, const int ke
 
 int main(int argc, const char * argv[])
 {
+	/*int result = 0;
+	int backResult = 0;
+	int num = 254;
+
+	result |= (num & 1) << 2;
+	result |= (num & 2) << 2;
+	result |= (num & 4) << 2;
+	result |= (num & 8) << 3;
+	result |= (num & 16) << 3;
+	result |= (num & 32) >> 5;
+	result |= (num & 64) >> 5;
+	result |= (num & 128) >> 2;
+	std::cout << int(result) << std::endl;
+
+	backResult |= (result & 1) << 5;
+	backResult |= (result & 2) << 5;
+	backResult |= (result & 4) >> 2;
+	backResult |= (result & 8) >> 2;
+	backResult |= (result & 16) >> 2;
+	backResult |= (result & 32) << 2;
+	backResult |= (result & 64) >> 3;
+	backResult |= (result & 128) >> 3;
+	std::cout << int(backResult) << std::endl;
+
+	_getch();*/
+
 	setlocale(LC_ALL, "Rus");
 	if (argc != 5)
 	{
@@ -54,9 +124,16 @@ int main(int argc, const char * argv[])
 		return 1;
 	}
 
-	if (!strcmp(argv[1], "crypt") || !strcmp(argv[1], "decrypt"))
+	if (!strcmp(argv[1], "crypt"))
 	{
-		if (Ñrypt(argv[2], argv[3], key))
+		if (WriteFromFileInFile(argv[2], argv[3], key, Ñrypt))
+		{
+			return 1;
+		}
+	}
+	else if (!strcmp(argv[1], "decrypt"))
+	{
+		if (WriteFromFileInFile(argv[2], argv[3], key, Decrypt))
 		{
 			return 1;
 		}
