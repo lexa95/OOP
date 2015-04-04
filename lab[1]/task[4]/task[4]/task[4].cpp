f8afa77ae9b0dd9595713fb77c6d0f6844cf8f36
lexa95 <lexashar@gmail.com>
2015-04-04 22:21:11 +0300

    замена fopen на if/ofstream

--- lab[1]/task[4]/task[4]/task[4].cpp
+++ lab[1]/task[4]/task[4]/task[4].cpp
@@ -1,83 +1,101 @@
 #include "stdafx.h"
 #include <conio.h>
 #include <iostream>
+#include <string>
+#include <fstream>
 
-void WriteInFile(FILE * f, int num1, int num2)
-{
-	fputc(num1, f);
-	fputc(num2, f);
-}
 
-bool Pack(const char * nameInputFile, const char * nameOutputFile)
+using namespace std;
+
+bool Pack(const string nameInputFile, const string nameOutputFile)
 {
-	FILE *pInputFile = fopen(nameInputFile, "rb");
-	if (pInputFile == NULL)
+	ifstream inputFile(nameInputFile, ios::binary | ios::in);
+	if (!inputFile.is_open())
 	{
-		printf("File opening error\n");
-		return true;
+		cout << "File opening error\n";
+		return false;
 	}
 
-	FILE *pOutputFile = fopen(nameOutputFile, "wb");
-	if (pOutputFile == NULL)
+	ofstream outputFile(nameOutputFile, ios::binary | ios::out);
+	if (!outputFile.is_open())
 	{
-		printf("File opening error\n");
-		return true;
+		cout << "File opening error\n";
+		return false;
 	}
-	
-	int ch, previous;
+
+	char ch;
+	char previous;
 	int index = 1;
-	previous = fgetc(pInputFile);
-	
-	while ((ch = fgetc(pInputFile)) != EOF)
+
+
+	inputFile.read((char*)&previous, sizeof(char));
+
+	while (!inputFile.eof())
 	{
-		if (ch == previous && index != 255)
+		inputFile.read((char*)&ch, sizeof(char));
+
+
+		if (ch == previous && index != 255 && !inputFile.eof())
 		{
 			index++;
 		}
 		else
 		{
-			WriteInFile(pOutputFile, index, previous);
+			//cout << ' ' << index << ' ' << previous;
+			outputFile.write((char*)&index, sizeof(char));
+			outputFile.write((char*)&previous, sizeof(char));
 			index = 1;
 		}
 		previous = ch;
 	}
-	WriteInFile(pOutputFile, index, previous);
 
-	fclose(pInputFile);
-	fclose(pOutputFile);
-	return false;
+	inputFile.close();
+	outputFile.close();
+
+	return true;
 }
 
-bool UnPack(const char * nameInputFile, const char * nameOutputFile)
+bool UnPack(const string nameInputFile, const string nameOutputFile)
 {
-	FILE *pInputFile = fopen(nameInputFile, "rb");
-	if (pInputFile == NULL)
+	ifstream inputFile(nameInputFile, ios::binary | ios::in);
+	if (!inputFile.is_open())
 	{
-		printf("File opening error\n");
-		return true;
+		cout << "File opening error\n";
+		return false;
 	}
 
-	FILE *pOutputFile = fopen(nameOutputFile, "wb");
-	if (pOutputFile == NULL)
+	ofstream outputFile(nameOutputFile, ios::binary | ios::out);
+	if (!outputFile.is_open())
 	{
-		printf("File opening error\n");
-		return true;
+		inputFile.close();
+		cout << "File opening error\n";
+		return false;
 	}
 
-	int ch;
-	int val;
-	while ((val = fgetc(pInputFile)) != EOF)
+	char ch;
+	char val;
+
+	while (!inputFile.eof())
 	{
-		ch = fgetc(pInputFile);
-		for (int i = 0; i < val; i++)
+		inputFile.read((char*)&val, sizeof(char));
+		inputFile.read((char*)&ch, sizeof(char));
+
+		if (inputFile.eof())
 		{
-			fputc(ch, pOutputFile);
+			continue;
+		}
+		
+		int length = unsigned char(val);
+
+		for (int i = 0; i < length; i++)
+		{
+			outputFile.write((char*)&ch, sizeof(char));
 		}
 	}
 
-	fclose(pInputFile);
-	fclose(pOutputFile);
-	return false;
+	inputFile.close();
+	outputFile.close();
+	return true;
 }
 
 int main(int argc, const char* argv[])
@@ -89,17 +107,17 @@ int main(int argc, const char* argv[])
 		std::cout << "Неверное кол-во параметров." << std::endl;
 		return 1;
 	}
-
+	
 	if (!strcmp(argv[1], "pack"))
 	{
-		if (Pack(argv[2], argv[3]))
+		if (!Pack(argv[2], argv[3]))
 		{
 			return 1;
 		}
 	}
 	else if (!strcmp(argv[1], "unpack"))
 	{
-		if (UnPack(argv[2], argv[3]))
+		if (!UnPack(argv[2], argv[3]))
 		{
 			return 1;
 		}
