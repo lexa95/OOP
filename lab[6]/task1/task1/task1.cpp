a35f79a3204f4d23e49713bcdb06ca73551151c8
lexa95 <lexashar@gmail.com>
2015-05-15 23:47:30 +0300

    объявление функции

---
+++ lab[6]/task1/task1/task1.cpp
@@ -0,0 +1,27 @@
+// task1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <math.h>
+
+struct EquationRoot4
+{
+	double numRoots;
+	double roots[4];
+};
+
+EquationRoot4 Solve4(double a, double b, double c, double d, double e)
+{
+	if (0 == a)
+	{
+		throw "\n a = 0";
+	}
+}
+
+int main(int argc, char* argv[])
+{
+
+
+	return 0;
+}
+
